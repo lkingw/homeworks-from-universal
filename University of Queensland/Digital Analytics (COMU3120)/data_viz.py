a8ba8a11c427c3c82aac3f7370f9d6e6884b5c59
Hongyu Jiang <1230223112@stu.cjlu.edu.cn>
2021-05-23 15:31:21 +0800

    update

--- University of Queensland/Digital Analytics (COMU3120)/data_viz.py
+++ University of Queensland/Digital Analytics (COMU3120)/data_viz.py
@@ -1,45 +1,146 @@
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-import json
+from plotly.subplots import make_subplots
+import statistics
 
-fig = make_subplots(rows=2, cols=3, 
-    specs=[[{"type":"polar"}, {"type":"polar"}, {"type":"polar"}], [{"type":"polar"}, {"type":"polar"}, {"type":"polar"}]])
+tracksFile = open('tracks.csv')
 
-years = ['1970', '1980', '1990', '2000', '2010', '2019']
-stats = []
+yearIndicator = dict()
 
-for index, year in enumerate(years):
-    text = open('data/' + year + '.json').read()
-    stats.append(json.loads(text))
+for line in tracksFile:
+    meta = line.split(',')
+    trackID = meta[3].replace('\n','')
+    year = meta[0]
+    yearIndicator[trackID] = year
 
-data = []
+featureFile = open('features.csv')
+fields = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 
+        'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
 
-for i in range(len(stats)):
-    data.append(dict())
-    for field in stats[i]:
-        if field not in ['count', 'tempo']:
-            data[i][field] = stats[i][field] / stats[i]['count']
-        if field == 'tempo':
-            data[i][field] = ((stats[i][field] - 60) / 60) / stats[i]['count']
+year_bucket = dict()
 
-categories = list(data[i].keys())
+for line in featureFile:
+    meta = line.split(',')
+    trackID = meta[11].replace('\n','')
+    year = yearIndicator[trackID]
+    block = dict()
 
-for i in range(len(data)):
-    d = data[i]
+    for i, d in enumerate(meta):
+        if i != 11:
+            block[fields[i]] = float(meta[i])
+            if fields[i] == 'tempo':
+                block[fields[i]] = (block[fields[i]] - 60) / 60
 
-    print(d.values())
+    if year not in year_bucket:
+        year_bucket[year] = []
+    year_bucket[year].append(block)
 
-    fig.add_trace(go.Scatterpolar(
-        r=list(d.values()),
-        theta=categories,
-        fill='toself',
-        name='Product A'
-    ),row=int(i/3) + 1, col=i%3 + 1)
-    
+'''
+fig = make_subplots(rows=2, cols=3, subplot_titles=("1970", "1980", "1990", "2000", "2010", "2019"))
+mat = dict()
 
-fig.update_layout(height=800, width=1600, title_text='Radar Plot of All Clusters (Fig.4)')
+for year in year_bucket:
+    data = year_bucket[year]
+    mat[year] = dict()
+    for field in fields:
+        mat[year][field] = []
+    for d in data:
+        for field in fields:
+            mat[year][field].append(d[field])
 
+maxF = dict()
+minF = dict()
+
+for year in mat:
+    for field in mat[year]:
+        maxV = max(mat[year][field])
+        minV = min(mat[year][field])
+        if field not in maxF or maxV > maxF[field]:
+            maxF[field] = maxV
+        if field not in minF or minV < minF[field]:
+            minF[field] = minV
+
+for year in mat:
+    for field in mat[year]:
+        maxV = maxF[field]
+        minV = minF[field]
+        for i in range(len(mat[year][field])):
+            mat[year][field][i] = (mat[year][field][i] - minV) / (maxV - minV)
+
+counter = 0
+
+for year in ['1970', '1980', '1990', '2000', '2010', '2019']:
+    for field in mat[year]:
+        fig.add_trace(go.Box(
+            y=mat[year][field],
+            name=field,
+            boxpoints='outliers', # only outliers
+            marker_color='rgb(107,174,214)',
+            line_color='rgb(107,174,214)'
+        ),row=int(counter/3) + 1, col=counter%3 + 1)
+    counter += 1
+
+fig.update_layout(title_text="Music features at different era", showlegend=False, height=800, width=1500)
 fig.show()
+'''
 
 
-#boxplot
+fig = make_subplots(rows=2, cols=3, subplot_titles=("1970", "1980", "1990", "2000", "2010", "2019"))
+mat = dict()
+
+for year in year_bucket:
+    data = year_bucket[year]
+    mat[year] = dict()
+    for field in fields:
+        mat[year][field] = []
+    for d in data:
+        for field in fields:
+            mat[year][field].append(d[field])
+
+# avg and std
+
+fieldDict = dict()
+
+for year in mat:
+    for field in mat[year]:
+        if field not in fieldDict:
+            fieldDict[field] = dict()
+        fieldDict[field][year] = mat[year][field]
+
+year_keys = sorted(list(mat.keys()), key = lambda x: int(x))
+
+avgValueDict = dict()
+
+for field in fieldDict:
+    for year in year_keys:
+        if field not in avgValueDict:
+            avgValueDict[field] = dict()
+        avgValueDict[field][year] = statistics.mean(fieldDict[field][year])
+
+stdValueDict = dict()
+
+for field in fieldDict:
+    for year in year_keys:
+        if field not in stdValueDict:
+            stdValueDict[field] = dict()
+        stdValueDict[field][year] = statistics.stdev(fieldDict[field][year])
+
+fig = make_subplots(rows=6, cols=2, subplot_titles=list(fieldDict.keys()))
+
+counter = 0
+for field in avgValueDict:
+    fig.add_trace(go.Scatter(
+        y=list(avgValueDict[field].values()),
+        x=list(avgValueDict[field].keys()),
+        name='AVG',
+        line_color='rgb(107,174,214)'
+    ),row=int(counter/2) + 1, col=counter%2 + 1)
+    fig.add_trace(go.Scatter(
+        y=list(stdValueDict[field].values()),
+        x=list(stdValueDict[field].keys()),
+        name='STDEV',
+        line_color='#44BD62'
+    ),row=int(counter/2) + 1, col=counter%2 + 1)
+    counter += 1
+
+fig.update_layout(title_text="Music features at different era", showlegend=True, height=2000, width=1200)
+fig.show()
